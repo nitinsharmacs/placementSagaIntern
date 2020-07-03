9d8788f87ccc902414bcfcd7c78a27af50fb2edd
nitinsharmacs <ramujha266@gmail.com>
2020-07-03 07:07:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import './assests/icons';
+
+class App extends Component{
+  render(){
+    return (
+  	<div className='App'>
+  		application
+  	</div>  
+  );
+  }
+}
+export default App;
---
+++ src/assests/icons.js
@@ -0,0 +1,4 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTimes, faTimesCircle, faCircleNotch, faCheckCircle} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faTimes, faTimesCircle, faCircleNotch, faCheckCircle);
